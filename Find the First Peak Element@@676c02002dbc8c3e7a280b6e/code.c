86f621ec5a5c95268d070f7a9d980daebca1a347
Arzookamboj1321 <arzookamboj1321@gmail.com>
2025-03-31 19:24:26 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,6 +1,3 @@
-// C program to find a peak element in the given array
-// Using Linear Search
-
 #include <stdio.h>
 
 int peakElement(int arr[], int n) {
@@ -8,17 +5,12 @@ int peakElement(int arr[], int n) {
         int left = 1;
         int right = 1;
 
-        // Check for element to the left
         if (i > 0 && arr[i] <= arr[i - 1])
             left = 0;
-
-        // Check for element to the right
         if (i < n - 1 && arr[i] <= arr[i + 1])
             right = 0;
-
-        // If arr[i] is greater than its left as well as
-        // its right element, return its index
-        if (left && right) {
+            if (left && right)
+             {
             return i;
         }
     }
